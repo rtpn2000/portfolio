e7495e7f2a55636338e19cae4b4c0c7024272d1b
rtpn2000 <rtpn2000@gmail.com>
2025-11-10 20:57:28 -0800

    Added the 'Tableau' visualizations as projects.

--- js/script.js
+++ js/script.js
@@ -151,6 +151,154 @@ window.addEventListener("click", (e) => {
   }
 });
 
+// Handling the 'Tableau Visualizations' Modal.
+// NBA Shot Chart Modal.
+const nbaModal = document.getElementById("nbaModal");
+const openNBAModal = document.getElementById("openNBAModal");
+const nbaCloseBtn = document.getElementById("nbaCloseBtn");
+
+// Refreshing the viz.
+openNBAModal.addEventListener("click", (e) => {
+  e.preventDefault();
+  nbaModal.style.display = "block";
+  const divElement = document.getElementById("vizNBA");
+  divElement.innerHTML = `<object class='tableauViz' style='display:none;'>
+    <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+    <param name='embed_code_version' value='3' />
+    <param name='site_root' value='' />
+    <param name='name' value='CPSC5320_VA3_NBA_Shotchart_Team3/NBAShotChart' />
+    <param name='tabs' value='no' />
+    <param name='toolbar' value='yes' />
+    <param name='static_image' value='https://public.tableau.com/static/images/CP/CPSC5320_VA3_NBA_Shotchart_Team3/NBAShotChart/1.png' />
+    <param name='animate_transition' value='yes' />
+    <param name='display_static_image' value='yes' />
+    <param name='display_spinner' value='yes' />
+    <param name='display_overlay' value='yes' />
+    <param name='display_count' value='yes' />
+    <param name='language' value='en-GB' />
+  </object>`;
+
+  setTimeout(loadTableauViz, 300);
+});
+
+nbaCloseBtn.addEventListener("click", () => {
+  nbaModal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === nbaModal) {
+    nbaModal.style.display = "none";
+  }
+});
+function loadTableauViz() {
+  const divElement = document.getElementById("vizNBA");
+  const vizElement = divElement.getElementsByTagName("object")[0];
+
+  vizElement.style.width = "100%";
+  vizElement.style.height = (divElement.offsetWidth > 800 ? divElement.offsetWidth * 0.75 :
+                            divElement.offsetWidth > 500 ? divElement.offsetWidth * 0.75 : 727) + "px";
+
+  const scriptElement = document.createElement("script");
+  scriptElement.src = "https://public.tableau.com/javascripts/api/viz_v1.js";
+  vizElement.parentNode.insertBefore(scriptElement, vizElement);
+}
+
+// Kanye West Twitter Analysis Modal.
+const kanyeModal = document.getElementById("kanyeModal");
+const openKanyeModal = document.getElementById("openKanyeModal");
+const kanyeCloseBtn = document.getElementById("kanyeCloseBtn");
+
+// Refreshing the viz.
+openKanyeModal.addEventListener("click", (e) => {
+  e.preventDefault();
+  kanyeModal.style.display = "block";
+  const divElement = document.getElementById("vizKanye");
+  divElement.innerHTML = `<object class='tableauViz' style='display:none;'>
+    <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+    <param name='embed_code_version' value='3' />
+    <param name='site_root' value='' />
+    <param name='name' value='TwitteratiReactionAnalysis_KanyeWest_VA7_Team3/TwitteratiReactionAnalysis' />
+    <param name='tabs' value='no' />
+    <param name='toolbar' value='yes' />
+    <param name='static_image' value='https://public.tableau.com/static/images/Tw/TwitteratiReactionAnalysis_KanyeWest_VA7_Team3/TwitteratiReactionAnalysis/1.png' />
+    <param name='animate_transition' value='yes' />
+    <param name='display_static_image' value='yes' />
+    <param name='display_spinner' value='yes' />
+    <param name='display_overlay' value='yes' />
+    <param name='display_count' value='yes' />
+    <param name='language' value='en-GB' />
+  </object>`;
+  setTimeout(loadKanyeViz, 300);
+});
+kanyeCloseBtn.addEventListener("click", () => {
+  kanyeModal.style.display = "none";
+});
+window.addEventListener("click", (event) => {
+  if (event.target === kanyeModal) {
+    kanyeModal.style.display = "none";
+  }
+});
+function loadKanyeViz() {
+  const divElement = document.getElementById("vizKanye");
+  const vizElement = divElement.getElementsByTagName("object")[0];
+
+  vizElement.style.width = "100%";
+  vizElement.style.height = (window.innerHeight * 0.85) + "px";
+
+  const scriptElement = document.createElement("script");
+  scriptElement.src = "https://public.tableau.com/javascripts/api/viz_v1.js";
+  vizElement.parentNode.insertBefore(scriptElement, vizElement);
+}
+
+// Disney Vacay Modal.
+const disneyModal = document.getElementById("disneyModal");
+const openDisneyModal = document.getElementById("openDisneyModal");
+const disneyCloseBtn = document.getElementById("disneyCloseBtn");
+
+// Refreshing the viz.
+openDisneyModal.addEventListener("click", (e) => {
+  e.preventDefault();
+  disneyModal.style.display = "block";
+  const divElement = document.getElementById("vizDisney");
+  divElement.innerHTML = `<object class='tableauViz' style='display:none;'>
+    <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+    <param name='embed_code_version' value='3' />
+    <param name='site_root' value='' />
+    <param name='name' value='WhenToPlanA_DisneyVacay/WaitTimeDashboard' />
+    <param name='tabs' value='no' />
+    <param name='toolbar' value='yes' />
+    <param name='static_image' value='https://public.tableau.com/static/images/Wh/WhenToPlanA_DisneyVacay/WaitTimeDashboard/1.png' />
+    <param name='animate_transition' value='yes' />
+    <param name='display_static_image' value='yes' />
+    <param name='display_spinner' value='yes' />
+    <param name='display_overlay' value='yes' />
+    <param name='display_count' value='yes' />
+    <param name='language' value='en-GB' />
+  </object>`;
+  setTimeout(loadDisneyViz, 300);
+});
+disneyCloseBtn.addEventListener("click", () => {
+  disneyModal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === disneyModal) {
+    disneyModal.style.display = "none";
+  }
+});
+
+function loadDisneyViz() {
+  const divElement = document.getElementById("vizDisney");
+  const vizElement = divElement.getElementsByTagName("object")[0];
+
+  vizElement.style.width = "100%";
+  vizElement.style.height = (window.innerHeight * 0.75) + "px";
+
+  const scriptElement = document.createElement("script");
+  scriptElement.src = "https://public.tableau.com/javascripts/api/viz_v1.js";
+  vizElement.parentNode.insertBefore(scriptElement, vizElement);
+}
+
 // Incrementing 'Like Counter'.
 document.addEventListener("DOMContentLoaded", function () {
   const likeBtn = document.getElementById("likeBtn");
